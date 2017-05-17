86939ee3bf57a3982a4ecc94a34e2df81fa35cbd
Stavros Kaparelos <skaparelos@gmail.com>
2017-05-18 01:54:46 +0300

    Create 112_best_time_to_buy_and_sell_stock_ii.java

---
+++ 112_best_time_to_buy_and_sell_stock_ii.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len == 0) return 0;
+        int sum = 0;
+        
+        for (int i = 0; i < len - 1; i++){
+            if (prices[i] < prices[i+1])
+                sum += prices[i+1] - prices[i];
+        }
+        
+        return sum;
+    }
+}
